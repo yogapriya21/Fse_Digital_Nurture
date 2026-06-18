b9633a319d5ae3b341156577102ed01505138a73
yogapriya21 <yogapriya210306@gmail.com>
2026-06-18 11:18:01 +0530

    Add files via upload

---
+++ Week1/FactoryPatternDemo.java
@@ -0,0 +1,13 @@
+public class FactoryPatternDemo {
+
+    public static void main(String[] args) {
+
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape shape1 = factory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = factory.getShape("RECTANGLE");
+        shape2.draw();
+    }
+}
